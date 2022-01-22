2bfc07aaf9cc282d24b47c28e6692f6643bf12bf
Hamad Hamdi <Hammock42@protonmail.com>
2022-01-22 12:44:26 +0100

    My first Git commit

---
+++ my_first_rep.py
@@ -0,0 +1,8 @@
+"""
+    Hamad Hamdi
+    First Git Repository Local Creation and Commit
+    January 22, 2022
+"""
+
+comment = "Just practicing Git commands"
+print(f"{comment} and Linux command line.")
